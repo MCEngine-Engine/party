c6ae6293880802d8c48b223151a8fc676f934d1e
JetsadaWijit <jetsadawijit@outlook.com>
2025-10-22 18:51:53 +0700

    feat: Add check license

--- server/papermc/engine/src/main/java/io/github/mcengine/papermc/party/engine/MCEnginePartyPaperMC.java
+++ server/papermc/engine/src/main/java/io/github/mcengine/papermc/party/engine/MCEnginePartyPaperMC.java
@@ -41,6 +41,15 @@ public class MCEnginePartyPaperMC extends JavaPlugin {
             return;
         }
 
+        String license = getConfig().getString("licenses.license", "free"); 
+        if (!license.equalsIgnoreCase("free")) { 
+            getLogger().warning("Plugin is disabled in config.yml.");
+            getLogger().warning("Invalid license.");
+            getLogger().warning("Check license or use \"free\".");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
         // =========================
         // Register MC Engine Party
         // =========================
